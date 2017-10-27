a4f04cdd5e4bf80547d731b8116dcecfc483dc08
Becky Segal <becsegal@gmail.com>
2017-10-26 23:42:34 -0400

    results display

--- solution/script.js
+++ solution/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   $('#searchForm').submit(function(e) {
-    console.log(e);
     e.preventDefault();
     searchGiphy();
   });
@@ -9,16 +8,12 @@ $(document).ready(function() {
 
 function searchGiphy() {
   var search_term = $('#searchField').val();
-  console.log("searching for " + search_term);
   var url = "http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=Rcx7a5vfUp1y6BZ1KjUtKLI2UovfkU3j";
   console.log("searching url: " + url);
   $.getJSON(url, function(results) {
-      console.log("results.data");
-      console.log(results.data);
       $('#results').empty();
-      for(var i=0; i< results.data[i].length; i++) {
-        console.log("data " + i);
-        $('#results').append("<img src='" + results[i].data.images.fixed_width.url + "' />");
+      for(var i=0; i< results.data.length; i++) {
+        $('#results').append("<img src='" + results.data[i].images.fixed_height.url + "' />");
       }
   });
 }
