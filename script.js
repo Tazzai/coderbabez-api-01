4107528c99a0d3522c2e5c613036d54bb78993f0
Tazzai <tazz@barkbox.com>
2017-11-07 18:38:55 -0500

    Added HTML, Some CSS and Some JS

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-script.js
+$(document).ready(function() {
+
+  $('#searchButton').click(function() {
+    console.log ('buttonClicked');
+    var q = $('#searchField').val ();
+    console.log (q)
+  });
+});
